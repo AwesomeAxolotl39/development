c80f1e5a83ee021864f081c95f7eaad57d61d166
Annabel Roth <annabelroth@annabels-mbp.devices.brown.edu>
2022-11-17 15:38:57 -0500

    trying to start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+bakeryData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+
+//1 sorting feature = time (vs popularity)
+//2 filtering cats = dairy free, gluten free
+//aggregator = favs section -- agg total cook time
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class='d-flex row'>
+        <h1>Recipe Finder</h1>
+        <div class='row row-cols-md-2'>
+            {bakeryData.map((item, index) => (
+              <BakeryItem item={item} />
+            ))}
+          </div>
+      </div>
     </div>
   );
 }
